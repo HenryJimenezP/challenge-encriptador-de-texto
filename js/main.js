7940d646bce43cb7afc0c24e8d80058463e6a60e
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2023-01-17 15:48:10 -0800

    Creando las constantes de los elementos a utilizar en main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const textareaMensaje = document.getElementById('textareaMensaje');
+const textareaResultado = document.getElementById('textareaResultado');
+const buttonEncriptar = document.getElementById('btnEncriptar');
+const buttonDesencriptar = document.getElementById('btnDesencriptar');
+const buttonCopiar = document.getElementById('btnCopiar');
