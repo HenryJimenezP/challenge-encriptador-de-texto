b2fa4830a2dfd700c7dab1bfd15d6f317a36a133
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2023-01-17 16:02:31 -0800

    Creando función encriptar

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ const textareaResultado = document.getElementById('textareaResultado');
 const buttonEncriptar = document.getElementById('btnEncriptar');
 const buttonDesencriptar = document.getElementById('btnDesencriptar');
 const buttonCopiar = document.getElementById('btnCopiar');
+
+function encriptar() {
+    var encriptarMensaje = textareaMensaje.value
+    .replaceAll('e', 'enter')
+    .replaceAll('i', 'imes')
+    .replaceAll('a', 'ai')
+    .replaceAll('o', 'ober')
+    .replaceAll('u', 'ufat');
+
+    textareaResultado.getElementById = 'textareaResultado';
+    textareaResultado.innerText = encriptarMensaje;
+    
+}
